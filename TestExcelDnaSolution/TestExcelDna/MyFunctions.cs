f60f8be3a8cce44ca34bfd6ec80267eaa9430d4d
Ivan Loginov <41970242+ilgnv88@users.noreply.github.com>
2022-03-26 22:04:41 +0300

    init excel-dna project with simple UDF

---
+++ TestExcelDnaSolution/TestExcelDna/MyFunctions.cs
@@ -0,0 +1,13 @@
+﻿using ExcelDna.Integration;
+
+namespace TestExcelDna
+{
+    public static class MyFunctions
+    {
+        [ExcelFunction(Description = "My first .NET function")]
+        public static string HelloDna(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
