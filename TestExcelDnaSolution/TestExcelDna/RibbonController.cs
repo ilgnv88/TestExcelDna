28a21228fa8ea795f4b5c8cfcdf70809dd51734c
Ivan Loginov <41970242+ilgnv88@users.noreply.github.com>
2022-03-28 00:42:51 +0300

    part_5

--- TestExcelDnaSolution/TestExcelDna/RibbonController.cs
+++ TestExcelDnaSolution/TestExcelDna/RibbonController.cs
@@ -1,9 +1,9 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Runtime.InteropServices;
 using ExcelDna.Integration;
 using ExcelDna.Integration.CustomUI;
-using System.Windows.Forms;
-using Microsoft.Vbe.Interop;
+using Microsoft.Office.Interop.Excel;
 using TestExcelDna.Properties;
 
 namespace TestExcelDna
@@ -43,8 +43,39 @@ namespace TestExcelDna
 
         public void OnButtonPressed(IRibbonControl control)
         {
-            MessageBox.Show("Hello from control " + control.Id);
-            DataWriter.WriteData();
+            Application xlApp = ExcelDnaUtil.Application as Application;
+
+            if (xlApp == null) return;
+
+            Workbook wb = xlApp.ActiveWorkbook;
+            if (wb == null) return;
+
+            Worksheet ws = wb.Worksheets.Add(Type: XlSheetType.xlWorksheet);
+            ws.Range["A1"].Value = "Date";
+            ws.Range["B1"].Value = "Value";
+
+            Range headerRow = ws.Range["A1", "B1"];
+            headerRow.Font.Size = 12;
+            headerRow.Font.Bold = true;
+
+            // Generally it's faster to write an array to a range
+            var values = new object[100, 2];
+            var startDate = new DateTime(2007, 1, 1);
+            var rand = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                values[i, 0] = startDate.AddDays(i);
+                values[i, 1] = rand.NextDouble();
+            }
+
+            ws.Range["A2"].Resize[100, 2].Value = values;
+            ws.Columns["A:A"].EntireColumn.AutoFit();
+
+            // Add a chart
+            Range dataRange = ws.Range["A1:B101"];
+            dataRange.Select();
+            ws.Shapes.AddChart(XlChartType.xlLineMarkers).Select();
+            xlApp.ActiveChart.SetSourceData(Source: dataRange);
         }
     }
 
