7402c6e5d2934e49e01470210f0b7977e7434454
Ivan Loginov <41970242+ilgnv88@users.noreply.github.com>
2022-03-27 23:24:26 +0300

    part_4

--- TestExcelDnaSolution/TestExcelDna/RibbonController.cs
+++ TestExcelDnaSolution/TestExcelDna/RibbonController.cs
@@ -1,28 +1,45 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using ExcelDna.Integration;
 using ExcelDna.Integration.CustomUI;
 using System.Windows.Forms;
+using Microsoft.Vbe.Interop;
+using TestExcelDna.Properties;
 
 namespace TestExcelDna
 {
-    // task 1
+    [ComVisible(true)]
     public class RibbonController : ExcelRibbon
     {
-        [ComVisible(true)]
-        public override string GetCustomUI(string ribbonId)
+        //public override string GetCustomUI(string ribbonId)
+        //{
+        //    return @"
+        //      <customUI xmlns='http://schemas.microsoft.com/office/2006/01/customui'>
+        //      <ribbon>
+        //        <tabs>
+        //          <tab id='tab1' label='My Tab'>
+        //            <group id='group1' label='My Group'>
+        //              <button id='button1' label='My Button' onAction='OnButtonPressed'/>
+        //            </group >
+        //          </tab>
+        //        </tabs>
+        //      </ribbon>
+        //    </customUI>";
+        //}
+       
+
+        public Bitmap MyLoadImage(IRibbonControl control)
         {
-            return @"
-              <customUI xmlns='http://schemas.microsoft.com/office/2006/01/customui'>
-              <ribbon>
-                <tabs>
-                  <tab id='tab1' label='My Tab'>
-                    <group id='group1' label='My Group'>
-                      <button id='button1' label='My Button' onAction='OnButtonPressed'/>
-                    </group >
-                  </tab>
-                </tabs>
-              </ribbon>
-            </customUI>";
+            System.Drawing.Icon icon = Properties.Resources.check;
+            switch (control.Id)
+            {
+                //case "RugbyImageButton": return icon.ToBitmap();
+                //case "button1": return TestExcelDna.Properties.Resources.check1;
+                case "button1": return icon.ToBitmap();
+                default: return null;
+            }
         }
+
 
         public void OnButtonPressed(IRibbonControl control)
         {
